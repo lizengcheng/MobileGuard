9c7c5c45f617b65102d5becdb1ab51af67224c12
lizengcheng <1986244354@qq.com>
2017-09-18 11:53:50 +0800

    0918

---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/utils/VersionEntity.java
@@ -0,0 +1,11 @@
+package cn.edu.gdmec.android.mobileguard.m1home.utils;
+
+/**
+ * Created by Administrator on 2017/9/17.
+ */
+
+public class VersionEntity {
+    public String versionCode;
+    public String description;
+    public String apkurl;
+}
