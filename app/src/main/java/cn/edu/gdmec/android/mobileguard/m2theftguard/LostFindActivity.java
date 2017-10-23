e0ff3db20cb12076243e0345c1983bba53fa1cbb
lizengcheng <1986244354@qq.com>
2017-10-24 00:09:49 +0800

    添加GPSLocationService.java,MyDeviceAdminReceiver.java,SmsLostFindReceiver.java文件（未写入代码），添加ylzs.mp3，添加device_admin_sample.xml(有问题),修改lostfindactivity.Java,setup4activity.java

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/LostFindActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/LostFindActivity.java
@@ -1,22 +1,48 @@
 package cn.edu.gdmec.android.mobileguard.m2theftguard;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import cn.edu.gdmec.android.mobileguard.R;
 
-public class LostFindActivity extends AppCompatActivity {
+public class LostFindActivity extends AppCompatActivity implements View.OnClickListener{
+    private TextView mStafePhoneTV;
+    private RelativeLayout mInterSetupRL;
+    private SharedPreferences mSharedPreferences;
+    private ToggleButton mToggleButton;
+    private TextView mProtecStatusTV;
 
+    private boolean isSetup(){
+        return mSharedPreferences.getBoolean("isSetup",false);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lost_find);
-        startSetUp1Activity();
+        mSharedPreferences = getSharedPreferences("config",MODE_PRIVATE);
+        if(!isSetup()) {
+            startSetUp1Activity();
+        }
+        initView();
+    }
+    private void initView(){
+        TextView mTitleTV = (TextView)findViewById(R.id.tv_title);
+        mTitleTV.setText("手机防盗");
     }
     public void startSetUp1Activity(){
         Intent intent = new Intent(LostFindActivity.this,Setup1Activity.class);
         startActivity(intent);
         finish();
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/Setup4Activity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/Setup4Activity.java
@@ -1,8 +1,12 @@
 package cn.edu.gdmec.android.mobileguard.m2theftguard;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.CompoundButton;
 import android.widget.RadioButton;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import cn.edu.gdmec.android.mobileguard.R;
 
@@ -11,17 +15,49 @@ import cn.edu.gdmec.android.mobileguard.R;
  */
 
 public class Setup4Activity extends BaseSetUpActivity {
+    private TextView mStatusTV;
+    private ToggleButton mToggleButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup_4);
         //设置第4个小圆点的颜色
         ((RadioButton)findViewById(R.id.rb_fourth)).setChecked(true);
+        initView();
+    }
+    private void initView(){
+        ((RadioButton)findViewById(R.id.rb_fourth)).setChecked(true);
+        mStatusTV = (TextView) findViewById(R.id.tv_setup4_status);
+        mToggleButton =(ToggleButton) findViewById(R.id.togglebtn_securityfunction);
+        mToggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    mStatusTV.setText("防盗保护已经开启");
+                } else {
+                    mStatusTV.setText("防盗保护没有开启");
+                }
+                SharedPreferences.Editor editor = sp.edit();
+                editor.putBoolean("protecting", isChecked);
+                editor.commit();
+            }
+        });
+        boolean protecting = sp.getBoolean("protecting",true);
+        if(protecting){
+            mStatusTV.setText("防盗保护已经开启");
+            mToggleButton.setChecked(true);
+        }else{
+            mStatusTV.setText("防盗保护没有开启");
+            mToggleButton.setChecked(false);
+        }
     }
 
     @Override
     public void showNext() {
-        Toast.makeText(this,"完成",Toast.LENGTH_LONG).show();
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putBoolean("isSetUp",true);
+        editor.commit();
+        startActivityAndFinishSelf(LostFindActivity.class);
     }
 
     @Override
